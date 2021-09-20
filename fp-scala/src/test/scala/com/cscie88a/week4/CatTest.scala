74a46bdd2fd837dedf4f2ec66a5a6e88fa88403a
esumitra <edward.sumitra@gmail.com>
2021-09-19 21:56:03 -0400

    scala week 4 scaffolding

---
+++ fp-scala/src/test/scala/com/cscie88a/week4/CatTest.scala
@@ -0,0 +1,40 @@
+package com.cscie88a.week4
+
+import com.cscie88a.testutils.StandardTest
+
+class CatTest extends StandardTest {
+
+  "Cat collections" when {
+    "calling averageAge" should {
+      "calculate average age in a list of cats with a single item" in {
+        false shouldBe true
+      }
+    }
+
+    "adding a new item" should {
+      "create a new list when an item in added to an existing list" in {
+        false shouldBe true
+      }
+    }
+
+    "calling averageCheckedAge" should {
+      "calculate average age only for cats with completed health checks" in {
+        false shouldBe true
+      }
+    }
+
+    "calling resetHealthCheck" should {
+      "set healthCheckCompleted to false for all cats" in {
+        false shouldBe true
+      }
+    }
+
+    "calling anonymizeData" should {
+      "return a list of tuples of (eye color, body color, age)" in {
+        false shouldBe true
+      }
+    }
+
+
+  }
+}
