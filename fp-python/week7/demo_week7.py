647fe9489f7c3b3a35bac0d3d2624836c0e0a10f
Marina Popova <ppine7@gmail.com>
2021-10-19 07:18:24 -0400

    week 7 code

--- fp-python/week7/demo_week7.py
+++ fp-python/week7/demo_week7.py
@@ -26,6 +26,23 @@ def my_map_fn(int_item):
         print("EXCEPTION in my_map_fn(): {}", format(str(e)))
 
 
+def demo_basic_operations():
+    animals = [
+        ("cat", "Sparky", 5),
+        ("dog", "Hero", 10),
+        ("dog", "Pirate", 3),
+        ("cat", "Lazy", 6),
+        ("cat", "Baby", 0.5)
+    ]
+    df = pd.DataFrame(animals, columns=['Type', 'Name', 'Age'])
+    print("df = \n{}".format(df))
+    print("Shape: {}".format(df.shape))
+    print("Size: {}".format(df.size))
+    print("Values: {}".format(df.values))
+    print("Head (1 row): {}".format(df.head(1)))
+    print("Tail (1 row): {}".format(df.tail(1)))
+
+
 def do_groupby_with_agg():
     animals = [
         ("cat", "Sparky", 5),
@@ -37,12 +54,17 @@ def do_groupby_with_agg():
     df = pd.DataFrame(animals, columns=['Type', 'Name', 'Age'])
     print("df = \n{}".format(df))
     groups_by_type = df.groupby(['Type'])
-    print("groups_by_type = \n")
+    print("groups_by_type = {}\n".format(groups_by_type))
     groups_by_type.apply(print)
+    print("groups: {}".format(groups_by_type.groups))
     groups_by_type_avg_age = groups_by_type.aggregate("mean")
     print("groups_by_type_avg_age = \n{}".format(groups_by_type_avg_age))
+    # same as using "mean"
+    groups_by_type_avg_age_np = groups_by_type.mean()
+    print("groups_by_type_avg_age_np = \n{}".format(groups_by_type_avg_age_np))
 
 
 if __name__ == "__main__":
-    do_pandas_simple()
-    #do_groupby_with_agg()
+    # demo_basic_operations()
+    # do_pandas_simple()
+    do_groupby_with_agg()
